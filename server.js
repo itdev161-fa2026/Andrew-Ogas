ec65111a4109ca0aff60dbededa53b0f68bf8d79
Andrew Ogas <ogasa@gmatc.matc.edu>
2025-09-11 19:11:20 -0500

    Updated Server

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ const app = express();
 //Connect to the database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({ extended: false}));
+
 // API endpoints
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register User
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
